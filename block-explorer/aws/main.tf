77f4601735e39580b35a9b65b660721bc57cd66f
mchrapek <mchrapek@evojam.com>
2019-11-13 08:20:54 +0100

    Change terraform to ec2 instead of lambda

--- block-explorer/aws/main.tf
+++ block-explorer/aws/main.tf
@@ -9,106 +9,54 @@ provider "aws" {
 */
 resource "aws_vpc" "vpc-block-explorer" {
   cidr_block = "10.1.0.0/16"
+
+  tags = {
+    Name = "block-explorer-vpc"
+  }
+}
+
+resource "aws_subnet" "subnet-block-explorer" {
+  vpc_id = "${aws_vpc.vpc-block-explorer.id}"
+  cidr_block = "10.1.0.0/24"
+  availability_zone = "eu-west-2a"
+
+  tags = {
+    Name = "block-explorer-subnet"
+  }
+}
+
+resource "aws_network_interface" "handler-app-block-explorer" {
+  subnet_id = "${aws_subnet.subnet-block-explorer.id}"
+  private_ips = ["10.1.0.10"]
+
+  tags = {
+    Name = "block-explorer-network-interface"
+  }
 }
 
 
 /*
-  Permissions for lambda function:
-    - to cloudwatch (logging)
-    - to S3
+  SQS Queue
 */
-resource "aws_iam_role" "iam-lambda-block-explorer" {
-  name               = "iam-for-block-explorer-lambda"
-  assume_role_policy = <<EOF
+resource "aws_sqs_queue" "queue-block-explorer" {
+  name = "s3-event-block-explorer-queue"
+
+  policy = <<POLICY
 {
   "Version": "2012-10-17",
   "Statement": [
     {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": "lambda.amazonaws.com"
-      },
-      "Effect": "Allow"
+      "Effect": "Allow",
+      "Principal": "*",
+      "Action": "sqs:SendMessage",
+      "Resource": "arn:aws:sqs:*:*:s3-event-block-explorer-queue",
+      "Condition": {
+        "ArnEquals": { "aws:SourceArn": "${aws_s3_bucket.bucket-block-explorer.arn}" }
+      }
     }
   ]
 }
-EOF
-}
-
-resource "aws_iam_policy" "lambda-logging-policy" {
-  name = "lambda-logging-policy"
-  path = "/"
-  description = "IAM policy for logging from a lambda"
-
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": [
-        "logs:CreateLogGroup",
-        "logs:CreateLogStream",
-        "logs:PutLogEvents"
-      ],
-      "Resource": "arn:aws:logs:*:*:*",
-      "Effect": "Allow"
-    }
-  ]
-}
-EOF
-}
-
-resource "aws_iam_policy" "lambda-s3-policy" {
-  name = "lambda-S3-block-explorer-bucket-policy"
-  path = "/"
-  description = "IAM policy for S3 block-explorer-bucket from a lambda"
-
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "s3:*",
-      "Resource": [
-        "arn:aws:s3:::block-explorer-bucket",
-        "arn:aws:s3:::block-explorer-bucket/*"
-      ],
-      "Effect": "Allow"
-    }
-  ]
-}
-EOF
-}
-
-resource "aws_iam_role_policy_attachment" "lambda_logs" {
-  role = "${aws_iam_role.iam-lambda-block-explorer.name}"
-  policy_arn = "${aws_iam_policy.lambda-logging-policy.arn}"
-}
-
-resource "aws_iam_role_policy_attachment" "lambda_s3" {
-  role = "${aws_iam_role.iam-lambda-block-explorer.name}"
-  policy_arn = "${aws_iam_policy.lambda-s3-policy.arn}"
-}
-
-
-/*
-  Permission to allow execution lambda function from a bucket
-*/
-resource "aws_lambda_permission" "allow-bucket-to-execute-lambda" {
-  statement_id  = "AllowExecutionFromS3Bucket"
-  action        = "lambda:InvokeFunction"
-  function_name = "${aws_lambda_function.lambda-block-explorer-s3-to-es-handler.arn}"
-  principal     = "s3.amazonaws.com"
-  source_arn    = "${aws_s3_bucket.bucket-block-explorer.arn}"
-}
-
-
-/*
-  Cloudwatch group
-*/
-resource "aws_cloudwatch_log_group" "cloud-watch-lambda-block-explorer" {
-  name              = "/aws/lambda/${aws_lambda_function.lambda-block-explorer-s3-to-es-handler.function_name}"
-  retention_in_days = 14
+POLICY
 }
 
 
@@ -122,27 +70,48 @@ resource "aws_s3_bucket" "bucket-block-explorer" {
 
 
 /*
-  S3 bucket event
+  S3 Bucket Notification
 */
 resource "aws_s3_bucket_notification" "bucket-notification-block-explorer" {
   bucket = "${aws_s3_bucket.bucket-block-explorer.id}"
 
-  lambda_function {
-    lambda_function_arn = "${aws_lambda_function.lambda-block-explorer-s3-to-es-handler.arn}"
-    events = ["s3:ObjectCreated:*"]
+  queue {
+    queue_arn     = "${aws_sqs_queue.queue-block-explorer.arn}"
+    events        = ["s3:ObjectCreated:*"]
   }
 }
 
 
 /*
-  Lambda function
+  EC2 Instance
 */
-resource "aws_lambda_function" "lambda-block-explorer-s3-to-es-handler" {
-  filename = "block-explorer-handler-lambda-assembly-1.0.0.jar"
-  function_name = "s3-to-es-handler-block-explorer"
-  handler = "org.constellation.handler.LambdaHandler::handleRequest"
-  role = "${aws_iam_role.iam-lambda-block-explorer.arn}"
-  runtime = "java8"
-  timeout = 240
-  memory_size = 1024
+data "aws_ami" "amzn2-ami" {
+  most_recent = true
+
+  filter {
+    name = "name"
+    values = ["amzn2-ami-hvm-2.0.*"]
+  }
+
+  filter {
+    name = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["amazon"]
+}
+
+
+resource "aws_instance" "handler-block-explorer" {
+  ami = "${data.aws_ami.amzn2-ami.id}"
+  instance_type = "t2.micro"
+
+  network_interface {
+    device_index = 0
+    network_interface_id = "${aws_network_interface.handler-app-block-explorer.id}"
+  }
+
+  tags = {
+    Name = "block-explorer-handler"
+  }
 }
