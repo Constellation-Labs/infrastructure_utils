b787dbf980144c5b68260c962cb30d1bbed130c3
Krystian Pudlik <kpudlik@evojam.com>
2020-07-08 13:02:23 +0200

    Get rid of SQS code

--- block-explorer/bucket.tf
+++ block-explorer/bucket.tf
@@ -9,11 +9,4 @@ resource "aws_s3_bucket" "bucket" {
   }
 }
 
-resource "aws_s3_bucket_notification" "bucket_notification" {
-  bucket = aws_s3_bucket.bucket.id
 
-  queue {
-    queue_arn     = aws_sqs_queue.queue.arn
-    events        = ["s3:ObjectCreated:*"]
-  }
-}
--- block-explorer/outputs.tf
+++ block-explorer/outputs.tf
@@ -2,10 +2,6 @@ output "security_group_id" {
   value = aws_security_group.security-group-access-to-vpc.id
 }
 
-output "sqs" {
-  value = data.aws_sqs_queue.sqsQueue.url
-}
-
 output "es" {
   value = aws_elasticsearch_domain.es-domain.endpoint
 }
