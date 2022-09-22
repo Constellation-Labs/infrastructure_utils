52b8734d511af622244b4226c4ec162152d19f19
Marcin Wadoń <mwadon@evojam.com>
2022-09-22 13:21:15 +0200

    Move bucket ACL to separate resource

--- snapshots-bucket/snapshots-bucket.tf
+++ snapshots-bucket/snapshots-bucket.tf
@@ -1,10 +1,14 @@
 resource "aws_s3_bucket" "cluster_snapshots" {
-    bucket = "constellationlabs-${var.env}-snapshots"
-    acl = "private"
+  bucket = "constellationlabs-${var.env}-snapshots"
 
   tags = {
     Env = var.env
     Name = "${var.env}-snapshots"
     Workspace = terraform.workspace
   }
+}
+
+resource "aws_s3_bucket_acl" "cluster_snapshots_acl" {
+  bucket = aws_s3_bucket.cluster_snapshots.id
+  acl = "private"
 }
