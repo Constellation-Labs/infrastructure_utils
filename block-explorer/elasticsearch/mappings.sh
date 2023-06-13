4ba1d0ba2b1343939614fd6083da0ee7107de4da
Krystian Pudlik <krystian.pudlik@gmail.com>
2023-06-13 13:18:02 +0200

    Mappings for Currency Framework

--- block-explorer/elasticsearch/mappings.sh
+++ block-explorer/elasticsearch/mappings.sh
@@ -7,4 +7,9 @@ curl -XPUT "$URL/blocks" -H 'Content-Type: application/json' -d '@./mappings/blo
 curl -XPUT "$URL/transactions" -H 'Content-Type: application/json' -d '@./mappings/transactions.json'
 curl -XPUT "$URL/balances" -H 'Content-Type: application/json' -d '@./mappings/balances.json'
 
+curl -XPUT "$URL/currency-snapshots" -H 'Content-Type: application/json' -d '@./mappings/currency-snapshots.json'
+curl -XPUT "$URL/currency-blocks" -H 'Content-Type: application/json' -d '@./mappings/currency-blocks.json'
+curl -XPUT "$URL/currency-transactions" -H 'Content-Type: application/json' -d '@./mappings/currency-transactions.json'
+curl -XPUT "$URL/currency-balances" -H 'Content-Type: application/json' -d '@./mappings/currency-balances.json'
+
 curl -XGET "$URL/_cat/indices"
